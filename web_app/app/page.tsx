407b60ed89a117eb6a2ce0b7f637a40882255ff2
Daniel Ezekiel <dbezekiel77@gmail.com>
2026-03-20 15:04:28 +0000

    feat: zeroshot classifier setup complete

--- web_app/app/page.tsx
+++ web_app/app/page.tsx
@@ -1,65 +1,96 @@
-import Image from "next/image";
+"use client";
+
+import { useState, useEffect, useRef, useCallback } from "react";
+
+interface WorkerMessage {
+  status: "initiate" | "ready" | "complete";
+  output?: unknown[];
+}
 
 export default function Home() {
+  /* TODO: Add state variables */ // Keep track of the classification result and the model loading status.
+  const [legalText, setLegalText] = useState<string>("");
+  const [result, setResult] = useState<null | unknown>(null);
+  const [ready, setReady] = useState<null | boolean>(null);
+
+  // Create a reference to the worker object.
+  const worker = useRef<null | Worker>(null);
+
+  // We use the `useEffect` hook to set up the worker as soon as the `App` component is mounted.
+  useEffect(() => {
+    if (!worker.current) {
+      // Create the worker if it does not yet exist.
+      worker.current = new Worker(new URL("./worker.ts", import.meta.url), {
+        type: "module",
+      });
+    }
+
+    // Create a callback function for messages from the worker thread.
+    const onMessageReceived = (e: MessageEvent<WorkerMessage>) => {
+      switch (e.data.status) {
+        case "initiate":
+          setReady(false);
+          break;
+        case "ready":
+          setReady(true);
+          break;
+        case "complete":
+          setResult(e.data);
+          console.log(e.data);
+          break;
+      }
+    };
+
+    // Attach the callback function as an event listener.
+    worker.current.addEventListener("message", onMessageReceived);
+
+    // Define a cleanup function for when the component is unmounted.
+    return () =>
+      worker.current?.removeEventListener("message", onMessageReceived);
+  });
+
+  const classify = useCallback((text: string) => {
+    if (worker.current) {
+      worker.current.postMessage({ text });
+    }
+  }, []);
+
   return (
-    <div className="flex min-h-screen items-center justify-center bg-zinc-50 font-sans dark:bg-black">
-      <main className="flex min-h-screen w-full max-w-3xl flex-col items-center justify-between py-32 px-16 bg-white dark:bg-black sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={100}
-          height={20}
-          priority
-        />
-        <div className="flex flex-col items-center gap-6 text-center sm:items-start sm:text-left">
-          <h1 className="max-w-xs text-3xl font-semibold leading-10 tracking-tight text-black dark:text-zinc-50">
-            To get started, edit the page.tsx file.
-          </h1>
-          <p className="max-w-md text-lg leading-8 text-zinc-600 dark:text-zinc-400">
-            Looking for a starting point or more instructions? Head over to{" "}
-            <a
-              href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-              className="font-medium text-zinc-950 dark:text-zinc-50"
-            >
-              Templates
-            </a>{" "}
-            or the{" "}
-            <a
-              href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-              className="font-medium text-zinc-950 dark:text-zinc-50"
-            >
-              Learning
-            </a>{" "}
-            center.
-          </p>
-        </div>
-        <div className="flex flex-col gap-4 text-base font-medium sm:flex-row">
-          <a
-            className="flex h-12 w-full items-center justify-center gap-2 rounded-full bg-foreground px-5 text-background transition-colors hover:bg-[#383838] dark:hover:bg-[#ccc] md:w-[158px]"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={16}
-              height={16}
-            />
-            Deploy Now
-          </a>
-          <a
-            className="flex h-12 w-full items-center justify-center rounded-full border border-solid border-black/8 px-5 transition-colors hover:border-transparent hover:bg-black/4 dark:border-white/[.145] dark:hover:bg-[#1a1a1a] md:w-[158px]"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Documentation
-          </a>
-        </div>
-      </main>
-    </div>
+    /* TODO: See below */
+    <main className="flex min-h-screen flex-col items-center justify-center p-12">
+      <h1 className="max-w-lg text-3xl font-bold mb-2 text-center">
+        Sovereignty Routing Algorithm (SRA) using Transformers.js
+      </h1>
+      <h2 className="max-w-lg text-xl mb-4 text-center">
+        Privacy-Preserving Hybrid Inference Architecture
+      </h2>
+
+      <textarea
+        className="w-full max-w-xl p-2 border border-gray-300 rounded mb-4"
+        placeholder="Enter text here"
+        value={legalText}
+        onChange={(e) => setLegalText(e.currentTarget.value)}
+      ></textarea>
+      {legalText && (
+        <button
+          type="button"
+          className="cursor-pointer px-4 py-2 border-2 border-gray-400 rounded-sm bg-slate-800 hover:bg-slate-700"
+          onClick={(e) => {
+            if (legalText) classify(legalText);
+            e.preventDefault();
+          }}
+        >
+          Classify Text
+        </button>
+      )}
+
+      <div className="mt-8">
+        {ready !== null && (
+        <pre className="max-w-180 bg-gray-800 p-2 rounded">
+          {!ready || !result ? "Loading..." : JSON.stringify(result, null, 2)}
+        </pre>
+      )}
+      </div>
+    </main>
   );
 }
---
+++ web_app/app/worker.ts
@@ -0,0 +1,78 @@
+import {
+  pipeline,
+  env,
+  PipelineType,
+  ProgressCallback,
+  ZeroShotClassificationPipeline,
+} from "@huggingface/transformers";
+
+// Skip local model check
+env.allowLocalModels = false;
+
+const NONE_LABEL = "Governing Law, Document Name, Parties...";
+
+const CLAUSE_LABELS = [
+  "Termination for Convenience",
+  "Non-compete",
+  "Uncapped Liability",
+  "Cap on Liability",
+  NONE_LABEL
+];
+
+// Use the Singleton pattern to enable lazy construction of the pipeline.
+class PipelineSingleton {
+  static task: PipelineType = "zero-shot-classification";
+  static model = "Xenova/distilbert-base-uncased-mnli";
+  static instance: ZeroShotClassificationPipeline | null = null;
+
+  static async getInstance(progress_callback: ProgressCallback | undefined) {
+    if (this.instance === null) {
+      this.instance = pipeline(this.task, this.model, { progress_callback }) as unknown as ZeroShotClassificationPipeline;
+    }
+    return this.instance;
+  }
+}
+
+// Listen for messages from the main thread
+self.addEventListener("message", async (event) => {
+  // Retrieve the classification pipeline. When called for the first time,
+  // this will load the pipeline and save it for future use.
+  const classifier = await PipelineSingleton.getInstance((x) => {
+    // We also add a progress callback to the pipeline so that we can
+    // track model loading.
+    self.postMessage(x);
+  });
+
+  const legal_text: string = event.data.text;
+
+  // Actually perform the classification
+  const output = await classifier(legal_text, CLAUSE_LABELS, {
+    multi_label: false,
+  }) as {labels: string[], scores: number[], sequence: string};
+
+  
+  const maxScore = Math.max(...output.scores);
+  const topLabelIdx = output.scores.indexOf(maxScore);
+  const topLabelScore = output["scores"][topLabelIdx]
+  const topLabel = output["labels"][topLabelIdx]
+  const isConfident = topLabelScore > 0.5
+  const isValidClauseForSRA = topLabel !== NONE_LABEL
+
+  console.log(topLabelIdx, topLabel, topLabelScore, isConfident, isValidClauseForSRA)
+
+  const results = {
+    topLabel,
+    topLabelScore,
+    isConfident,
+    isValidClauseForSRA,
+    routeToCloud: !isValidClauseForSRA || !isConfident,
+    allLabels: output.labels,
+    allScores: output.scores,
+  }
+
+  // Send the output back to the main thread
+  self.postMessage({
+    status: "complete",
+    results,
+  });
+});
